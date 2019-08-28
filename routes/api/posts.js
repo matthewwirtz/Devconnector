705a84661c8367144d7909039f051387d01c0d65
Matthew Wirtz <matthew.wayne.wirtz@gmail.com>
2019-08-28 01:15:07 -0700

    Set up add, get, and delete POST routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -9,7 +9,7 @@ const Post = require('../../models/Post');
 
 
 // @route   POST api/posts
-// @desc    Test route
+// @desc    Create a post
 // @access  Private
 router.post('/', [auth, [
   check('text', 'Text is required').not().isEmpty()
@@ -32,6 +32,65 @@ router.post('/', [auth, [
     res.json(post);
   } catch (err) {
     console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
+
+// @route   GET api/posts
+// @desc    Get all posts
+// @access  Private
+router.get('/', auth, async (req, res) => {
+  try {
+    const posts = await Post.find().sort({ date: -1 });
+    res.json(posts);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+
+});
+
+// @route   GET api/posts/:id
+// @desc    Get post by id
+// @access  Private
+router.get('/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    if(!post) {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+    res.json(post);
+  } catch (err) {
+    console.error(err.message);
+    if(err.kind === 'ObjectId') {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
+    res.status(500).send('Server Error');
+  }
+
+});
+
+// @route   DELETE api/posts/:id
+// @desc    Delete a post
+// @access  Private
+router.delete('/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id);
+
+    // Check if correct user is deleting their post
+    if (post.user.toString() !== req.user.id) {
+      return res.status(403).json({ msg: 'User not authorized' });
+    }
+
+    await post.remove();
+
+    res.json({ msg: 'Post removed' });
+  } catch (err) {
+    console.error(err.message);
+    if(err.kind === 'ObjectId') {
+      return res.status(404).json({ msg: 'Post not found' });
+    }
     res.status(500).send('Server Error');
   }
 });
--- routes/api/profile.js
+++ routes/api/profile.js
@@ -321,7 +321,7 @@ router.get('/github/:username', async (req, res) => {
       if(error) console.error(error);
 
       if(response.statusCode !== 200) {
-        return res.status(400).json({ msg: 'No Github profile found' });
+        return res.status(404).json({ msg: 'No Github profile found' });
       }
       res.json(JSON.parse(body));
     });
