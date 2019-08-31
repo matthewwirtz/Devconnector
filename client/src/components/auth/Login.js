d9bc202526a871ef6ad1817f8881d463be0d429e
Matthew Wirtz <matthew.wayne.wirtz@gmail.com>
2019-08-31 16:04:08 -0700

    Created a redux store

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,5 +1,5 @@
 import React, { Fragment, useState } from "react";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 const Login = () => {
   const [formData, setFormData] = useState({
@@ -11,12 +11,11 @@ const Login = () => {
 
   const onChange = e =>
     setFormData({ ...formData, [e.target.name]: e.target.value });
-
+    
   const onSubmit = async e => {
     e.preventDefault();
-    console.log('SUCCESS');
-  }
-  
+    console.log("SUCCESS");
+  };
 
   return (
     <Fragment>
