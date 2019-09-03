e060a9675f575fcef53623a001fad5099d1ba844
Matthew Wirtz <matthew.wayne.wirtz@gmail.com>
2019-09-02 20:09:35 -0700

    Able to edit profile

--- client/src/reducers/profile.js
+++ client/src/reducers/profile.js
@@ -28,7 +28,7 @@ export default function(state = initialState, action) {
         ...state,
         profile: null,
         repos: [],
-        loading: false
+        loading: true
       };
     default:
       return state;
