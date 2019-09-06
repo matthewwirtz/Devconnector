c253699152aabf27e0bca592e697c725467d0b8d
Matthew Wirtz <matthew.wayne.wirtz@gmail.com>
2019-09-05 19:46:50 -0700

    Cleaning things up

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -159,7 +159,6 @@ router.get("/:user_id", async (req, res) => {
 
     res.json(profile);
   } catch (err) {
-    console.error('ERRRRRRRRRROR');
     console.error(err.message);
     if (err.kind == "ObjectId") {
       return res.status(400).json({ msg: "Profile not found" });
